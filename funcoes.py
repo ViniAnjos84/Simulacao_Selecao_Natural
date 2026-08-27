f186be832bfc9a7e67400398bdcebe2660a948fa
Vinicius Anjos <113810181+ViniAnjos84@users.noreply.github.com>
2026-08-26 23:49:29 -0300

    Remove sistema de temperatura

--- funcoes.py
+++ funcoes.py
@@ -17,24 +17,3 @@ def exibe_mensagem(msg, tamanho, cor):
     texto_recortado = texto_formatado.subsurface(bounding).copy()
 
     return texto_recortado
-
-
-def cor_grama(temperatura):
-    if temperatura <= 15:
-        # -10°C → 15°C
-        t = (temperatura + 10) / 25
-        cor_fria = (180, 200, 180)
-        cor_normal = (50, 150, 50)
-    else:
-        # 15°C → 40°C
-        t = (temperatura - 15) / 25
-        cor_normal = (50, 150, 50)
-        cor_quente = (190, 150, 40)
-        cor_fria = cor_normal
-        cor_normal = cor_quente
-
-    r = int(cor_fria[0] + (cor_normal[0] - cor_fria[0]) * t)
-    g = int(cor_fria[1] + (cor_normal[1] - cor_fria[1]) * t)
-    b = int(cor_fria[2] + (cor_normal[2] - cor_fria[2]) * t)
-
-    return (r, g, b)
